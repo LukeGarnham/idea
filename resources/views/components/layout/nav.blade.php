84a0c8742fdacd515b2b8fcb5b2804b42cad0afd
Luke Garnham <lukegarnham@msn.com>
2026-03-22 18:30:17 +0000

    Created registration and login views. Added controllers to successfully register new users, and log them in and out.

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,21 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="/" class="flex items-center gap-x-2">
+                <img src="/images/logo.png" alt="Idea logo" width="50"><span class="italic text-xl font-bold">Idea</span>
+            </a>
+        </div>
+        <div class="flex gap-x-5">
+            @auth
+                <form method="POST" action="/logout">
+                    @csrf
+                    @method('DELETE')
+                    <button class="">Log Out</button>
+                </form>
+            @else
+                <a href="/login" class="">Sign In</a>
+                <a href="/register" class="btn">Register</a>
+            @endauth
+        </div>
+    </div>
+</nav>

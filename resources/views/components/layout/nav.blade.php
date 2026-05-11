202efe4aaad1c2aff5282cdfaddfb55bb20fb21f
Luke Garnham <lukegarnham@msn.com>
2026-05-11 16:27:31 +0100

    Added a view and functionality for users to update their profile. Written a (basic) test for this.

--- resources/views/components/layout/nav.blade.php
+++ resources/views/components/layout/nav.blade.php
@@ -7,6 +7,7 @@
         </div>
         <div class="flex gap-x-5">
             @auth
+                <a href="{{ route('profile.edit') }}" class="">Edit Profile</a>
                 <form method="POST" action="/logout">
                     @csrf
                     @method('DELETE')
---
+++ tests/Browser/ProfileTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\User;
+use App\Notifications\EmailChanged;
+use Illuminate\Support\Facades\Notification;
+
+it('requires authentication', function () {
+    visit(route('profile.edit'))->assertPathIs('/login');
+    // This is the same test as above except a browser is not opened.
+    $this->get(route('profile.edit'))->assertRedirect(('/login'));
+});
+
+it('edits a profile', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+    visit(route('profile.edit'))
+        ->assertValue('name', $user->name)
+        ->fill('name', 'New Name')
+        ->assertValue('email', $user->email)
+        ->fill('email', 'new@example.com')
+        ->click('Update Account')
+        ->assertSee('Profile updated!');
+
+    expect($user->fresh())->toMatchArray([
+        'name' => 'New Name',
+        'email' => 'new@example.com',
+    ]);
+});
+
+it('notifies the original email if updated', function () {
+    $user = User::factory()->create();
+    $originalEmail = $user->email;
+    $this->actingAs($user);
+    Notification::fake();
+    visit(route('profile.edit'))
+        ->assertValue('email', $user->email)
+        ->fill('email', 'new@example.com')
+        ->click('Update Account')
+        ->assertSee('Profile updated!');
+    Notification::assertSentOnDemand(EmailChanged::class, function (EmailChanged $notification, $routes, $notifiable) use ($originalEmail) {
+        return $notifiable->routes['mail'] === $originalEmail;
+    });
+});
